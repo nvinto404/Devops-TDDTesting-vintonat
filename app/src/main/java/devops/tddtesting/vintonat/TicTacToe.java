74040c633bd80e6b53b1ae98cf0e73a91251d366
nvinto404 <80949076+nvinto404@users.noreply.github.com>
2024-04-02 17:41:53 +0200

    Methode isColumnTaken wurde hinzugefügt um das Spiel zu beenden sobald alle Felder in einer Spalte liegen. Weiteres wurde die isGameOver Methode erweitert.

--- app/src/main/java/devops/tddtesting/vintonat/TicTacToe.java
+++ app/src/main/java/devops/tddtesting/vintonat/TicTacToe.java
@@ -24,12 +24,21 @@ public class TicTacToe {
     }
 
     public boolean isGameOver() {
-        return isRowTaken();
+        return isRowTaken() || isColumnTaken();
     }
 
     private boolean isRowTaken() {
         for (int i = 0; i < 3; i++) {
             if (board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isColumnTaken() {
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i] != ' ' && board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
                 return true;
             }
         }
